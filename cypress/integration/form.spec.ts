7ad1b229a04f7d7248ac0e8a6ff3549a888df872
John C. Kennedy <devjckennedy@gmail.com>
2021-02-16 13:43:56 +0100

    refactor test to use support functions

--- cypress/integration/form.spec.ts
+++ cypress/integration/form.spec.ts
@@ -1,29 +1,27 @@
+import * as faker from "faker"
+import { enterEmail } from "../support/enter-email"
+import { enterMessage } from "../support/enter-message"
+import { enterName } from "../support/enter-name"
+
 describe("Form test", () => {
+  beforeEach(() => {
+    cy.visit("/")
+  })
+
   it("Can fill the form", () => {
-    cy.visit("/");
-    cy.get("form");
-
-    cy.get('input[name="name"]')
-      .type("Molly")
-      .should("have.value", "Molly");
-
-    cy.get('input[name="email"]')
-      .type("molly@dev.io")
-      .should("have.value", "molly@dev.io");
-
-    cy.get("textarea")
-      .type("Mind you if I ask some silly question?")
-      .should("have.value", "Mind you if I ask some silly question?");
+    enterName(faker.name.firstName(0))
+    enterEmail(faker.internet.email())
+    enterMessage(faker.lorem.paragraph())
 
     // Old API in use here (Cypress <= 4.9.0) - use .intercept() instead
-    cy.server();
+    cy.server()
     cy.route({
       url: "/users/**",
       method: "POST",
-      response: { status: "Form saved!", code: 201 }
-    });
+      response: { status: "Form saved!", code: 201 },
+    })
 
-    cy.get("form").submit();
-    cy.contains("Form saved!");
-  });
-});
+    cy.get("form").submit()
+    cy.contains("Form saved!")
+  })
+})
